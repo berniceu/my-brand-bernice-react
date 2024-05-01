a91466f120e77e6c86f6c935453e4591801a113f
bernice uwituze <b.uwituze@alustudent.com>
2024-05-01 16:15:48 +0200

    add login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
 import './signup.css';
 import moon from '../images/moon.png';
 import { useNavigate } from "react-router-dom";
 
+const LoginPost = () => {
 
+    const [login, setLogin ] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+
+        const formData = {
+            email: form.email.value,
+            password: form.password.value
+
+        }
+        try{
+            const res = await fetch('https://my-brand-api-x8z4.onrender.com/users/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            })
+
+            if(res.ok){
+                const data = await res.json();
+                const userRole = data.role;
+                alert('logged in successfully')
+
+                if(userRole === 'admin'){
+                    navigate('/adminpanel')
+                } else{
+                    navigate('/blogs')
+                }
+            } else{
+                setError('Login failed')
+            }
+
+        } catch(err){
+            setError(err);
+            
+        } finally{
+            setLoading(false);
+        }
+
+    }
+
+    return(<>
+    <form onSubmit={handleLogin} id="login-form">
+            <div className="input-field">
+                <input type="email" placeholder="Email Address" className="item" id="email" autocomplete="email"/>
+                <div className="error-text">Email Address can't be blank</div>
+            </div>
+            
+            <div className="input-field">
+                <input type="password" placeholder="Password" className="item" id="login-password" autocomplete="current-password"/>
+                <div className="error-text">Password can't be blank</div>
+            </div>
+
+            
+            <button type="submit" id="login-button">{loading? "Logging In":"Log In"}</button>
+        </form>
+        
+        
+            
+    </>)
+}
 
 const Login = () => {
     const navigate = useNavigate();
@@ -20,23 +85,8 @@ const Login = () => {
         <div className="title">
             <h1>LOG IN</h1>
         </div>
-        <form action="" id="login-form">
-            <div className="input-field">
-                <input type="email" placeholder="Email Address" className="item" id="email" autocomplete="email"/>
-                <div className="error-text">Email Address can't be blank</div>
-            </div>
-            
-            <div className="input-field">
-                <input type="password" placeholder="Password" className="item" id="login-password" autocomplete="current-password"/>
-                <div className="error-text">Password can't be blank</div>
-            </div>
-
-            
-            
-        </form>
+        <LoginPost/>
         
-        
-            <button type="submit" id="login-button" onClick={() => {navigate('/adminpanel')}}>Log In</button>
         
     </div>
     
